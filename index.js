c5c7a0c6671fddb6ecd8c47e87ca4e1607efbe91
George Duncan <gtduncan@usc.edu>
2022-06-07 00:03:41 -0700

    commit tree 754873da38dcbe4724f4cd6b867e843d9d5e27fa

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(nameArray, event)
+{
+    let newArray = [];
+    for(let i=0; i<nameArray.length;i++)
+    {
+        newArray.push(`Thank you, ${nameArray[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArray;
+}
+
+function countDown(num)
+{
+    while(num>=0)
+    {
+        console.log(num)
+        num--;
+    }
+}
+
+countDown(10);
